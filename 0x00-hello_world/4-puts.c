35934b51647cb07fde50614dddd77ee46050f899
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-02 12:09:54 -0700

    Printing to standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* A simple code that prints a string to standard output*/
+
+	int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}
