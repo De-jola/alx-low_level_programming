f532d0416d0b134a4032665ad0c42cf571605398
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-03 13:16:29 -0700

    Permutation and combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	n = '0';
+	while (n >= '0' && n <= '9')
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
