bc6b0dbe7f9ef70a20ab121794cf0b3d2e2d168d
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-03 15:10:43 -0700

    More combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = '0';
+
+	while (n < '9')
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			
+			if (m != n)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
