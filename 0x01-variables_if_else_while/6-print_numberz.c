9765e26ad32d321d38e9452fc14c178dea0cbea2
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-03 12:10:05 -0700

    Printing integers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	n = '0';
+	while (n >= '0' && n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
