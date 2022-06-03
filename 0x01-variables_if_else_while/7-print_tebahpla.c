0b5a605b51b4a10d583cb15ce0c7284db6fd12cb
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-03 12:23:22 -0700

    The alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char a;
+
+	a = 'z';
+	while (a >= 'a' && a <= 'z')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
