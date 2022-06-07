6d21b54815b665a5ac0fd43dbca8fdbe74b24798
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-07 12:28:49 -0700

    Finding lowercase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase letters
+ * @c: character to be checked
+ *
+ * Return: Returns the value of check
+ */
+
+int _islower(int c)
+{
+	int check;
+
+	if (c >= 97 && c <= 122)
+		check = 1;
+	else if (c >= 65 && c <= 90)
+		check = 0;
+	return (check);
+}
