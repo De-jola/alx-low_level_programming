1abaacec1f0e7bb0c4a8407eb8117cc883236ae6
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-07 16:12:46 -0700

    Last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number
+ *
+ * @n: the number to be examined
+ *
+ * Return: Returns the last digits of the number
+ */
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+	if(m < 0)
+	{
+		_putchar(-m + 48);
+		return (-m);
+	}
+	else
+	{
+		_putchar(m + 48);
+		return(m);
+	}
+}
