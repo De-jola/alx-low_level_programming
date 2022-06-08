3a9f0aaaefc26b3f085d5c9921356df675a0bf8b
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-07 17:03:26 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers and prints the result
+ *
+ * @n: character to be added
+ * @m: character to be added
+ * Return: Value of addition
+ */
+int add(int n, int m)
+{
+	int c;
+
+	c = n + m;
+	return (c);
+}
