d58a5890519b9ab229350da6bc49cea0f66abc40
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-09 11:58:44 -0700

    A function that print lines based on the number inputted

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  print_line - prints a line a particular number of times
+ *  @n: the number of lines to be printed
+ */
+void print_line(int n)
+{
+	char c;
+	int i;
+
+	c = '_';
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		_putchar(c);
+	}
+	_putchar('\n');
+}
