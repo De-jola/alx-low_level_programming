44503fd3e87977a770ddedaa1d6a81b487a78f48
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-13 09:58:30 -0700

    Counts the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - counts the length of a string
+ * @s: the pointer to the variable to be counted
+ * Return: return the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i <= *(s + i); i++)
+	{
+		if (*s == '\"')
+			continue;
+		else
+			sum += 1;
+	}
+	return (sum);
+}
