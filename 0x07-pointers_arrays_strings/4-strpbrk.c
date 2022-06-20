c939608b0d9358a87c13ce0a786389d3bc555fb3
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-20 12:58:56 -0700

    Created own strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - locates the first occurrence in the string s
+ * of any of the bytes in the string accept
+ * @s: The string to be checked against
+ * @accept: The string to be checked
+ * Return: A pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0' && s[i] != accept[i]; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (0);
+}
