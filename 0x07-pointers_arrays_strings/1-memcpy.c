dead8bffb254b43034156f9d342ebb971ab83bcb
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-20 09:15:04 -0700

    Created a memcpy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: Memory area to be copied to
+ * @src: Memory area to be copied from
+ * @n: The number of bytes of memory area to be copied
+ * Return: A pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (*(src + i) != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
