fc51fe9be624a8a2197ffdd13b161f0a39498e58
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-22 14:34:39 -0700

    Palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -31,3 +31,14 @@ int is_palindrome_rec(char *s, int i, int j)
 		return (0);
 	return (is_palindrome_rec(s, i + 1, j - 1));
 }
+/**
+ * _strlen_recursion - finds the length of a string
+ * @s: string
+ * Return: The length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
