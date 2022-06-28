b7d1e0aa27f2744d6f3236c9a4e93b11cb903e99
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-28 11:15:01 -0700

    More memory allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer which contains a duplicate of
+ * the string given as a parameter
+ * @str: The string ti be duplicated
+ * Return: A pointer to a string
+ */
+
+char *_strdup(char *str)
+{
+	char *c;
+	int i, j, len;
+
+	len = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	c = malloc(sizeof(char) * len);
+	if (str == NULL || c == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	while (str[j] != '\0')
+	{
+		c[j] = str[j];
+		j++;
+	}
+	return (c);
+}
