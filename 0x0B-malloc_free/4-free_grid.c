f2bad6afd9a82442dd30d1a38f5ce9192edc43a5
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-06-28 16:49:59 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grind - frees the memory
+ * @grid: The array to be removed from memory
+ * @height: Height of the array
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(t[i]);
+	free(t);
+}
