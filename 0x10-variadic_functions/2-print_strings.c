57113b54a5f939f0a5bc459d237e22c0cd90eb38
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-07 14:25:46 -0700

    Print Strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 		{
 			printf("%s", ptr);
-			if (i < n -1 && separator != NULL)
+			if (i < n - 1 && separator != NULL)
 				printf("%s", separator);
 		}
 	}
