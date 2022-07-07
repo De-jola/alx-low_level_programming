fcbe01b374305e8ee9697e73048d2fa5047d7e09
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-06 23:53:34 -0700

    Integers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - searches an integer
+ * @array: pointer array
+ * @size: number elements in array
+ * @cmp: pointer to a function
+ * Return: First element index, on success or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] != 0)
+			return (i);
+
+	}
+		return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,5 @@
 void print_name(char *, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_elem(int elem);
+int int_index(int *, int, int (*cmp)(int));
 #endif
