6d7a36f919bb0ca4b0b637ff2b904c7833b0a44a
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-06 23:43:14 -0700

    Arrays

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
+#include <stddef.h>
 #ifndef HEADERGUARD
 #define HEADERGUARD
 void print_name(char *, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_elem(int elem);
 #endif
