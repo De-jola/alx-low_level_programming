85a772a281723181a9615b4c8fd51048960094e9
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-07 11:11:20 -0700

    Sum

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ..);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct typ - struct operators
+ *
+ * @c: Char
+ * @tp: The function associated
+ */
+typedef struct typ
+{
+	char *c;
+	void (*tp)(va_list var);
+} typ_t;
+#endif
