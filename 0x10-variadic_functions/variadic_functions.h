f9a6abb6bc553acd91e77c7eeb24a17e3c76bb0d
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-07 12:30:31 -0700

    Sum

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-int sum_them_all(const unsigned int n, ..);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
